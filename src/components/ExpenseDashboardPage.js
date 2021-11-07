35123736449b1bb607cf694d202deb731fc3263a
atb1234 <41204391+atb1234@users.noreply.github.com>
2021-11-07 03:26:13 +0200

    Initial commit

---
+++ src/components/ExpenseDashboardPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import ExpenseList from './ExpenseList'
+import ExpenseLisFilters from './ExpenseListFilters'
+
+
+const ExpenseDashboardPage = () => (
+    <div>
+      <ExpenseLisFilters />
+      <ExpenseList />
+    </div>
+)
+
+export default ExpenseDashboardPage
